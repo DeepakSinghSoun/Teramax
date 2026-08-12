66cb1c91d6d7e52f3dc9311831bae99965e6c100
onlyvedansh <vedanshsinghkarki@gmail.com>
2026-08-12 14:39:31 +0530

    django start project

--- mainProject/urls.py
+++ mainProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for teramax_project project.
+URL configuration for mainProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
