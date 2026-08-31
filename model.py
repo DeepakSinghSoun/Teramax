68ac15c771213cd1538bd04fda64c94d29ae614d
onlyvedansh <vedanshsinghkarki@gmail.com>
2026-08-31 23:32:18 +0530

    added accuracy score and train test split to random forest model to impprove model

--- model.py
+++ model.py
@@ -8,16 +8,20 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-
+from sklearn.metrics import accuracy_score 
 df = pd.read_csv("exampleDataset.csv")
 
 x=df.drop(columns=["ipaddr","name","suspecious"])
 y=df["suspecious"]
 
 
-model=RandomForestClassifier(random_state=42)
+X_train , X_test , y_train , y_test = train_test_split(x,y,test_size=0.2 , random_state=42)
 
 
-model.fit(x,y)
+model=RandomForestClassifier(random_state=42 , class_weight="balanced")
 
-print("traineddd")
+
+model.fit(X_train , y_train)
+y_predict=model.predict(X_test)
+
+print(f"MODEL ACCURACY : {accuracy_score(y_test , y_predict)*100:.2f}% \n")
