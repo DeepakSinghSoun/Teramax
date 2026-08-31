7ce39068c0c3c60ef102538b3b34b07e6bb31d5e
onlyvedansh <vedanshsinghkarki@gmail.com>
2026-08-31 22:25:03 +0530

    something fucked up with model.py

--- model.py
+++ model.py
@@ -4,3 +4,20 @@
 """model alert ->show alert in dashboard"""
 """model automate -> flag and fix sus devices"""
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("exampleDataset.csv")
+
+x=df.drop(columns=["ipaddr","name","suspecious"])
+y=df["suspecious"]
+
+
+model=RandomForestClassifier(random_state=42)
+
+
+model.fit(x,y)
+
+print("traineddd")
